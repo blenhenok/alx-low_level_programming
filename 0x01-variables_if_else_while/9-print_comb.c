d655e274a7371fbd92e772a1773b4385ae4a176d
blenhenok <blenhenok93@gmail.com>
2023-02-20 12:40:43 -0800

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all combinations of single digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchart(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+	}
