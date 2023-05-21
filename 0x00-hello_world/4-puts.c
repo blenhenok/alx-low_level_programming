33f6bfe928f541c77257dd3aaee106010b71f0db
blen <blenhenok93@gmail.com>
2023-05-21 10:35:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,6 @@
  */
 int main(void)
 {
-	char string[] = ""Programming is like building a multilingual puzzle";
-
-	puts(string);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 	}
