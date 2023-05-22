d3a95a56de366e2d4ca9adbc2a2727007f94c7fc
blen <blenhenok93@gmail.com>
2023-05-22 03:01:25 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+ * main - A program that prints the size of various data types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char: %lu" sizeof(a));
+	printf("size of an int: %lu" sizeof(b));
+	printf("size of a long int: %lu" sizeof(c));
+	printf("size of a long long int: %lu" sizeof(d));
+	printf("size of a float: %lu" sizeof(e));
+	return (0);
+}
