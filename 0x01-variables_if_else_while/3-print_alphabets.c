fcc3cd1fee4ed146287ece2d5011f99fdc569765
blen <blenhenok93@gmail.com>
2023-05-22 09:41:51 -0700

    lower and upper cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a, A'; letter <= 'z, Z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
