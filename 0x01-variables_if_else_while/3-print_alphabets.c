5ff219db54d063035131f6e43266bfe0d84b8a5e
blen <blenhenok93@gmail.com>
2023-05-22 09:21:17 -0700

    upper case alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'A'; letter <= 'Z'; letter++)
+	for (letter = 'a, A'; letter <= 'z, Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);
