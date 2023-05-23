f1ccb9085c954c2076c01a3eacf8571d87ff9452
blen <blenhenok93@gmail.com>
2023-05-23 10:58:28 -0700

    check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: An input character
+ * Return: 1 if is lowercase or 0 otherwise
+ */
+int _islower(int c)
+{
+	char letter;
+	int j = 0;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == c)
+			j = 1;
+	}
+	return (lower);
+}
