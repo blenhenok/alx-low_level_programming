40452959d69086aaa1215662a676a2a321ca06d9
blen <blenhenok93@gmail.com>
2023-05-23 11:22:52 -0700

    check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: An input character
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int j = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				j == 1;
+		}
+	}
+	return (j);
+}
