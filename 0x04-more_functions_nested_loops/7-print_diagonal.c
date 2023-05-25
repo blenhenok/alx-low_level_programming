a3688f5f1c912b03d0bbe7838becadc8f7e937f6
blen <blenhenok93@gmail.com>
2023-05-25 08:35:44 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * @n: The character
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
