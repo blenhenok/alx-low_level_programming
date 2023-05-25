dc6e8454923cb20f4f59abffa545505afaa4a364
blen <blenhenok93@gmail.com>
2023-05-25 11:14:06 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,8 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1; j < (size - i); j++)
-		{
 			_putchar(' ');
-		}
+		for (j--; j < size; j++)
 		_putchar('#');
 		if (i < (size - 1))
 			_putchar('\n');
