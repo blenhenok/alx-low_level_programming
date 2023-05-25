8c93b708e51010adef5ca93b4ecbd21f2d4a8d0c
blen <blenhenok93@gmail.com>
2023-05-25 11:51:06 -0700

    prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,15 +5,20 @@
  */
 int main(void)
 {
-	long n = 612852475143; 
+	long n = 612852475143;
 	int i;
 
-	for (i = 2; i <= n; i++)
+	while (i++ < n / 2)
 	{
 		if (n % i == 0)
 		{
-			n /= i;
-			i--;
+			n /= 2;
+			continue;
+		}
+		for (i = 3; i < n / 2; i += 2)
+		{
+			if (n % i == 0)
+				n /= i;
 		}
 	}
 	printf("%ld\n", n);
