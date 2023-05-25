e34cfb582650bdeb7383582f87bb3eeaaaa5ce54
blen <blenhenok93@gmail.com>
2023-05-25 10:53:21 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 		if (n == 1)
 			printf("%d", n);
 		else if ((n % 3 == 0) && (n % 5 == 0))
-			printf(" FizzBizz");
+			printf(" FizzBuzz");
 		else if (n % 3 == 0 && n % 5 != 0)
 			printf(" Fizz");
 		else if (n % 5 == 0 && n % 3 != 0)
