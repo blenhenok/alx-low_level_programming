31ace9e740b4c811dcc87081894b4f73098b9b9d
blen <blenhenok93@gmail.com>
2023-05-25 07:00:01 -0700

    nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,10 @@
  *@c: An input character
  *Return: 1 or 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	char letter;
-	int j = 0;
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	{
-		if (letter == c)
-			j == 1;
-	}
-	return (j);
+	if ( c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
