cdd678e84e4fd1e44c72b6a2800acb7642d02ed2
blen <blenhenok93@gmail.com>
2023-05-25 07:04:27 -0700

    nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
- *_isupper - Entry point
- *@c: An input character
- *Return: 1 or 0
+ * _isupper - Entry point
+ * @c: A character to print
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
