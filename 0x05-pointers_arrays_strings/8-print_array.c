aa642a579a746e05f2acf7cda2d0b3f0728164bc
blen <blenhenok93@gmail.com>
2023-05-30 11:14:08 -0700

    number of arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - Entry point
+ * @a: the character
+ * @n: number of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
