fe9f8916558c57e7f6d4afff05a9d53be4844aa6
blen <blenhenok93@gmail.com>
2023-06-05 06:04:52 -0700

    locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: entry string
+ * @needle: substring
+ * Return: pointer to substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack, *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
+}
