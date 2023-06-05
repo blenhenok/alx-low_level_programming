d4a351b05e40ac042381e1b23afa41b23cecfb60
blen <blenhenok93@gmail.com>
2023-06-05 06:37:25 -0700

    matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: of matrix
+ * Return: no return
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			s1 += *(a + 1);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			s2 += *(a + 1);
+	}
+	printf("%d, %d\n", s1, s2);
+}
