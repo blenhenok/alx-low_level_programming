ce3d259733a17b8f0b99eae998c63e1c3d18aa0f
blen <blenhenok93@gmail.com>
2023-06-05 06:08:13 -0700

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
 			haystack++;
-			pneedle;
+			pneedle++;
 		}
 		if (!*pneedle)
 			return (bhaystack);
