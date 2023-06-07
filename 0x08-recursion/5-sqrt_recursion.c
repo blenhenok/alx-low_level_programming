4c9a1b953dd4b2f1013105ed82e739327aace1b7
blen <blenhenok93@gmail.com>
2023-06-07 11:41:06 -0700

    square root"
    i
    cd
    git xommit
    clear
    exit
    >
    "

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * power_operation - Entry point
+ * @n: input
+ * @c: iterator
+ * Return: square
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - Entry point
+ * @n: input
+ * Return: square
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
+
