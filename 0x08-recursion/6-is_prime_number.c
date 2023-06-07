b7177e382f38cda0f83b3d095ab42461a7266c6f
blen <blenhenok93@gmail.com>
2023-06-07 11:57:23 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * is_prime - Entry point
+ * @n: input
+ * @c: iterator
+ * Return: prime
+ */
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - Entry point
+ * @n: input
+ * Return: square
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
